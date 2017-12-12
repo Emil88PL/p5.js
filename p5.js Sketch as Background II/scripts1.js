595f9a9bac20dc95049f9960441442c75001e7df
Emil <emil88pl@gmail.com>
2017-12-12 16:37:43 +0000

    Add files via upload

---
+++ p5.js Sketch as Background II/scripts1.js
@@ -0,0 +1,28 @@
+let canvas;
+let mic;  // mic
+
+function windowResized() {
+  // console.log('resized');
+  resizeCanvas(windowWidth,windowHeight);
+}
+
+function setup() {
+  canvas =  createCanvas(windowWidth,windowHeight);
+  canvas.position(0,0);
+//  canvas.style('z-index', '-1');
+
+//  mic = new p5.AudioIn(); //mic
+//  mic.start();            //mic
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+  }
+//  let vol = mic.getLevel(); //mic
+//  ellipse(width/2, height/2, vol * windowWidth); //mic
+}
+
+function keyPressed() {
+    clear();
+}
